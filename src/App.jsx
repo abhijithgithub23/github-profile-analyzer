00130f5a88555435444c19ff5f0b28160602bbcf
Abhijith <abhijith23ksd@gmail.com>
2026-03-18 18:39:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-500xl mt-9 font-bold text-blue-500">
+        Tailwind Working
+      </h1>
+    </>
+  )
+}
+
+export default App
+
